2f11f1e469fc51f7274f361810d04facd7787979
Cozma Calin <calinzabalt@gmail.com>
2024-07-03 08:59:13 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,16 +3,6 @@ import 'firebase/firestore'
 import 'firebase/auth'
 import 'firebase/storage'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAnsJuP1MsAhU2gzdrHKlZSM0ZzzyZVhfE",
-    authDomain: "stag-v3-d8b3e.firebaseapp.com",
-    projectId: "stag-v3-d8b3e",
-    storageBucket: "stag-v3-d8b3e.appspot.com",
-    messagingSenderId: "1063286158883", 
-    appId: "1:1063286158883:web:ba68904ca0a9946e76e396",
-    measurementId: "G-26Z9ND8ZPG"
-};
-
 // init firebase
 firebase.initializeApp(firebaseConfig)
 
